fc51564b4b67b440ae6cf8cacba996ec325d5d53
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2020-12-17 13:48:44 -0500

    completed first endpoint to get an account based on id

--- api/server.js
+++ api/server.js
@@ -3,7 +3,50 @@ const express = require("express");
 const db = require("../data/dbConfig.js");
 
 const server = express();
-
 server.use(express.json());
+// general error
+
+server.use((req,res,next,err)=>{
+    console.log(err)
+    res.status(500).json({message:'Something wrong'})
+    next()
+  })
+
+
+server.get('/:id', async (req,res,next)=>{
+    let id = req.params.id
+  
+
+    try{
+        const account = await db.select("*").from("accounts").where("id",id)
+         res.status(200).json(account[0])
+     
+    }
+    catch(err){next(err)}
+})
+
+server.post('/',(req,res,next)=>{
+
+    try{
+
+    }
+    catch(err){next(err)}
+})
+
+server.put('/:id',(req,res,next)=>{
+
+    try{
+
+    }
+    catch(err){next(err)}
+})
+
+server.delete('/:id',(req,res,next)=>{
+
+    try{
+
+    }
+    catch(err){next(err)}
+})
 
 module.exports = server;
